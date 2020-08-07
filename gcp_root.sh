b8f7baafc82943ebe8df476aa7b21d02480723d4
hqhyco <62784346+hqhyco@users.noreply.github.com>
2020-08-07 15:37:41 +0800

    Delete gcp_root.sh

--- gcp_root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "输入你的root密码(eg: 123456): " rootPwd
-echo root:$rootPwd |sudo chpasswd root
-sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo service sshd restart
