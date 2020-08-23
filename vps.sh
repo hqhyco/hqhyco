a695a6fed25f3a9a1d5e26ba2e57e9d53dd39e7a
hqhyco <62784346+hqhyco@users.noreply.github.com>
2020-08-23 15:50:53 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -19,7 +19,7 @@ cd /root
 if [-e "tcp.sh"]; then
 ./tcp.sh
 else
-wget -N --no-check-certificate "https://raw.githubusercontent.com/hqhyco/Linux-NetSpeed/master/tcp.sh" 
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 chmod u+x tcp.sh 
 ./tcp.sh
 fi
